d12515d9b3eef644abfdb6d8559d262ff4f7159f
Laryssa Rocha <laryssa.vrocha@gmail.com>
2024-11-04 20:28:04 -0300

    refactor: reorder hooks and remove clashing classes

--- src/components/PokemonList/PokemonList.tsx
+++ src/components/PokemonList/PokemonList.tsx
@@ -4,29 +4,31 @@ import React, { useCallback } from "react";
 import InfiniteLoader from "react-window-infinite-loader";
 import { FixedSizeList as List, ListChildComponentProps } from "react-window";
 
-import { useFetchPokemon } from "../../hooks/useFetchPokemon";
 import { Pokemon } from "../../types/pokemon";
 
 import styles from "./PokemonList.module.css";
 import PokemonCard from "../PokemonCard/PokemonCard";
 import useAvailableHeight from "../../hooks/useAvailableHeight";
+import { Filters } from "../Filters";
+import { usePokemonList } from "../../hooks/usePokemonList";
 
 const ROW_HEIGHT = 170;
 const LIST_WIDTH = "100%";
 
 export const PokemonList: React.FC = () => {
-  const { pokemonList, hasNextPage, isNextPageLoading, loadNextPage, error } =
-    useFetchPokemon();
+  const {
+    pokemons: pokemonList,
+    loadMorePokemons,
+    hasMore,
+    isLoading,
+    error,
+  } = usePokemonList();
   const { parentElementRef, height } = useAvailableHeight();
 
-  const itemCount = hasNextPage ? pokemonList.length + 1 : pokemonList.length;
-
   const isItemLoaded = useCallback(
-    (index: number) => !hasNextPage || index < pokemonList.length,
-    [hasNextPage, pokemonList]
+    (index: number) => !hasMore || index < pokemonList.length,
+    [hasMore, pokemonList]
   );
-
-  const loadMoreItems = isNextPageLoading ? () => {} : loadNextPage;
 
   const Row = useCallback(
     ({ index, style }: ListChildComponentProps) => {
@@ -37,6 +39,7 @@ export const PokemonList: React.FC = () => {
           </div>
         );
       }
+
       const pokemons: Pokemon[] = pokemonList[index];
 
       return (
@@ -50,30 +53,34 @@ export const PokemonList: React.FC = () => {
     [isItemLoaded, pokemonList]
   );
 
+  const itemCount = hasMore ? pokemonList.length + 1 : pokemonList.length;
+  const loadMoreItems = isLoading ? () => {} : loadMorePokemons;
+
   return (
-    <div className={styles.container} ref={parentElementRef}>
-      {error && <div className={styles.error}>{error}</div>}
-      <InfiniteLoader
-        isItemLoaded={isItemLoaded}
-        itemCount={itemCount}
-        loadMoreItems={loadMoreItems}
-        threshold={10}
-      >
-        {(props) => (
-          <List
-            height={height}
-            itemCount={itemCount}
-            itemSize={ROW_HEIGHT}
-            width={LIST_WIDTH}
-            {...props}
-          >
-            {Row}
-          </List>
-        )}
-      </InfiniteLoader>
-      {isNextPageLoading && (
-        <div className={styles.loading}>Loading more Pokémon...</div>
-      )}
+    <div>
+      <Filters />
+      <div className={styles.container} ref={parentElementRef}>
+        {error && <p className={styles.error}>{error}</p>}
+        <InfiniteLoader
+          isItemLoaded={isItemLoaded}
+          itemCount={itemCount}
+          loadMoreItems={loadMoreItems}
+          threshold={10}
+        >
+          {(props) => (
+            <List
+              height={height}
+              itemCount={itemCount}
+              itemSize={ROW_HEIGHT}
+              width={LIST_WIDTH}
+              {...props}
+            >
+              {Row}
+            </List>
+          )}
+        </InfiniteLoader>
+        {isLoading && <p className={styles.loading}>Loading Pokémons...</p>}
+      </div>
     </div>
   );
 };
